b32dd91772a285c1da908352ac1719acbac11445
eliiizabethmorales <103603541+eliiizabethmorales@users.noreply.github.com>
2022-04-13 17:07:10 -0400

    variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+numero = 3
+print(numero)
+
+numero1 = 5
+numero2 = 3
+print(numero1 + numero2)
+
+numero = numero1 ** numero2 
+print(numero)
+
+# reglas: variables no pueden contener espacios
+# reglas : la variable no puede empezar por un numero
+# reglas: la variable no puede contener signos especiales
+# reglas: siempre escribir variables en minusculas, evitar que partan en mayuscula
+# reglas: para separar palabras se escribe con un _
+numero_resultante = numero1 * numero2
